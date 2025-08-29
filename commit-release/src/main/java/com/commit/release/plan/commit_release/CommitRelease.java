261fac7c6605617d3e2d257dc8636f208187f53d
Ragul D <85984148+imragul26@users.noreply.github.com>
2025-08-29 05:39:14 +0000

    initial commit

---
+++ commit-release/src/main/java/com/commit/release/plan/commit_release/CommitRelease.java
@@ -0,0 +1,13 @@
+package com.commit.release.plan.commit_release;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CompanyController {
+
+    @GetMapping("/company")
+    public String getDetails() {
+        return "Welcome to Acme Corporation!";
+    }
+}

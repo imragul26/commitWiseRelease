ae851b9938dc5c5efc9e42cf2ad9ed94cd457126
Ragul D <85984148+imragul26@users.noreply.github.com>
2025-08-29 05:20:19 +0000

    initial-commit

---
+++ commit-release/src/main/java/com/commit/release/plan/commit_release/CommitReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.commit.release.plan.commit_release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitReleaseApplication.class, args);
+	}
+
+}
---
+++ commit-release/src/test/java/com/commit/release/plan/commit_release/CommitReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commit.release.plan.commit_release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
